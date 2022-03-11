4e62f7c338e2078f4858764af36fbf6eeb3d65ac
MattLawson98 <matthewlawson50@gmail.com>
2022-03-10 16:50:42 -0800

    This file can take an array and compy it down until hitting a specific condition, it will then return what has been copied!

---
+++ takeUntil.js
@@ -0,0 +1,44 @@
+const takeUntil = function(array, callback) {
+  let result = [];
+  for(const val of array){
+    if(!callback(val)){
+      result.push(val);   
+    } 
+    else{
+      return result;
+    }
+  }
+  return result;
+};
+
+function eqArrays(arr1,arr2){
+  for(i = 0; i < arr1.length; i++){
+    if(arr1[i] !== arr2[i]){
+      return false;
+    }
+  }
+  return true
+}
+const assertArrayEqual = function(arr1, arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
+  }
+  else { 
+    console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}`)
+  }
+  
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+
+assertArrayEqual(results1,[ 1, 2, 5, 7, 2 ]);
+assertArrayEqual(results2,[ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+assertArrayEqual(results1,[ 12, 2, 35, 7, 2 ]);

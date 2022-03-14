78363e4cae312a1ee9c73d0084b833ca94a08173
MattLawson98 <matthewlawson50@gmail.com>
2022-03-14 15:15:41 -0700

    This is the refactored version of middle, it will take an array and return the middle elements!

--- middle.js
+++ middle.js
@@ -1,21 +1,3 @@
-function eqArrays(arr1,arr2){
-  for(i = 0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i]){
-      return false;
-    }
-  }
-  return true
-}
-const assertArrayEqual = function(arr1, arr2) {
-  if (eqArrays(arr1,arr2)) {
-    console.log(`✅✅✅Assertion Passed: ${arr1} === ${arr2}`);
-  }
-  else { 
-    console.log(`🛑🛑🛑Assertion Failed: ${arr1} !== ${arr2}`)
-  }
-  
-};
-
 function middle(arr1){
   let newArr = [];
   for(i = 0; i < arr1.length; i++){
@@ -31,6 +13,5 @@ function middle(arr1){
   }
   return newArr;
 }
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3])) // => [2]
+
+module.exports = middle;

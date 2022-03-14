d494c86d73a1765ecf0f23288ac81a3134800be8
MattLawson98 <matthewlawson50@gmail.com>
2022-03-14 15:25:20 -0700

    This file uses chai to test the head function

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,18 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-const words = ["Hey", "Lighthouse", "Labs"];
-assertEqual(head(words), "Hey"); 
+// const words = ["Hey", "Lighthouse", "Labs"];
+// assertEqual(head(words), "Hey"); 
+
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns Hey for [Hey, Lighthouse, Labs]", () => {
+    assert.strictEqual(head(["Hey", "Lighthouse", "Labs"]), "Hey");
+  });
+});

f6af1b11abd75b06c5ffb6567c7151470af4a6e2
MattLawson98 <matthewlawson50@gmail.com>
2022-03-14 15:36:28 -0700

    This file uses chai to test the middle.js function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,19 @@
-const assertArrayEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
 
-assertArrayEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]) // => [3, 4]
-assertArrayEqual(middle([1, 2, 3, 4, 5]),[3]) // => [3]
-assertArrayEqual(middle([1, 2, 3]),[2]) // => [2]
+// assertArrayEqual(middle([1, 2, 3, 4, 5, 6]),[3,4]) // => [3, 4]
+// assertArrayEqual(middle([1, 2, 3, 4, 5]),[3]) // => [3]
+// assertArrayEqual(middle([1, 2, 3]),[2]) // => [2]
+
+describe("#middle", () => {
+  it("returns [3,4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4])
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3])
+  });
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2])
+  });
+})

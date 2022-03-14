6f462c3a52c80874620fbdb4beefed7b2dd43b2b
MattLawson98 <matthewlawson50@gmail.com>
2022-03-14 15:36:56 -0700

    This file uses chai to test the tail.js function

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,11 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail')
-let words = ["Lighthouse", "Bootcamp"]
-assertEqual(tail(words), "Bootcamp");
+
+// let words = ["Lighthouse", "Bootcamp"]
+// assertEqual(tail(words), "Bootcamp");
+
+describe("#tail", () => {
+  it("returns Bootcamp for [Lighthouse, Bootcamp]", () => {
+    assert.strictEqual(tail(["Lighthouse", "Bootcamp"]), "Bootcamp")
+  });
+})

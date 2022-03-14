b4e13b9d7a5a058da5ba6fddd8b86687a4022bc2
MattLawson98 <matthewlawson50@gmail.com>
2022-03-14 14:54:13 -0700

    This is the refactored version of eqArrays, it will compare to arrays to see if they are the same

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,20 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  }
-  else { 
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`)
-  }
-  
-};
 function eqArrays(arr1,arr2){
-  for(i = 0; i < arr1.length; i++){
+  if(arr1.length !== arr1.length) {
+    return false;
+  }
+  for(let i in arr1){
+    if(Array.isArray(arr1[i])){
+      if(!(eqArrays(arr1[i],arr2[i]))){
+        return false;
+      }
+    }
     if(arr1[i] !== arr2[i]){
       return false;
-    }
+    } 
   }
   return true
-}
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+};
+
+
+
+module.exports = eqArrays;

9c399364ee8d3aa1971bd9bd86cd92c0e7c12c8c
MattLawson98 <matthewlawson50@gmail.com>
2022-03-14 14:48:55 -0700

    This file will return the last element of an array

--- tail.js
+++ tail.js
@@ -1,11 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  }
-  else { 
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`)
-  }
-  
-};
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+const tail = function (arr) {
+return arr[arr.length - 1];
+}
+
+module.exports = tail;
